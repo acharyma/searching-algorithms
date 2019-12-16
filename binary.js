755673fd358ea0cf61ffd29f3ad628d16f41020e
Manogya Acharya <acharyma@bc.edu>
2019-12-15 19:21:13 -0500

    Final commit

--- binary.js
+++ binary.js
@@ -192,9 +192,6 @@ function reveal(){
     if(show == 0){
         table2.style.visibility = "visible";
         compare2.style.visibility = "visible";
-        if(val<arrays[0][0]){
-          show+=50;
-        }
         show+=1;
     }
     else if(show==1){
@@ -207,16 +204,10 @@ function reveal(){
         else{
           show+=1;
         }
-        if(val<arrays[1][0]){
-          show+=50;
-        }
     }
     else if(show==2){
         table4.style.visibility = "visible";
         compare4.style.visibility="visible";
-        if(val<arrays[2][0]){
-          show+=50;
-        }
         show+=1;
     }
     console.log("where: "+where)
